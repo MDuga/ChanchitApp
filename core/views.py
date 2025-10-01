5df39b2982f81983a2cad56cf7d84be4ad8ef727
Mariana Duga <mdugadiaz@gmail.com>
2025-09-30 23:40:31 -0300

    Se fragmenta view - query_mes usando funciones auxiliares. Se debe ver si se puede evitar la repeticion de código. Se modifica template de inicio para evitar repetir código, está pendiente revisar view - pagina_inicio para evitar repetur código.

--- core/views.py
+++ core/views.py
@@ -69,10 +69,18 @@ def pagina_inicio(request):
        
     saldo_actual = ingresos_actual + egresos_actual
     
+    #CARDS para template
+
+    cards = [
+        {"img": "Ingresos.png", "alt": "Ingresos", "title": "Ingresos", "text": "Registra entradas de dinero", "url": "ingreso_monto"},
+        {"img": "Egresos.png", "alt": "Egresos", "title": "Egresos", "text": "Registra salidas de dinero", "url": "egreso_monto"},
+        {"img": "Consultas.png", "alt": "Consultas", "title": "Consultas", "text": "Realiza consulta de movimientos", "url": "query_mes"},
+    ]
 
     contexto = {
         "usuario": usuario,                
-        "saldo_actual": saldo_actual
+        "saldo_actual": saldo_actual,
+        "cards": cards
     }
    
     return render(request, 'core/panel/inicio.html', contexto)
@@ -115,88 +123,116 @@ def egreso_monto(request):
     return render(request, "core/movimientos/egresos_form.html", {"form": formulario, "usuario": usuario})
 
 #====================================================================
-
-def query_mes(request):    
+#Auxiliar para query_mes - Nº1
+def obtener_usuario(request):
     usuario_id = request.session.get("usuario_id")
-    usuario = Usuario.objects.get(id=usuario_id)
+    return Usuario.objects.get(id=usuario_id)
+    
+#=================================================
+#Auxiliar para query_mes - Nº2
+def fecha_inicio_mes(request):
+    month_year = QueryMes_Form(request.GET or None)
+    if request.method == "GET" and month_year.is_valid(): 
+    # if request.GET and month_year.is_valid(): 
+        month = month_year.cleaned_data["month"]
+        year = month_year.cleaned_data["year"]  
+        date_inicio_mes = date(year, month, 1)
+    
+        return date_inicio_mes, year, month, month_year
+    
+    return date(1, 1, 1), 1, 1, month_year    # se pone como fecha 01/01/01 porque con None da error
+
+#=================================================
+#Auxiliar para query_mes - Nº3
+def saldo_inicial_query(request, usuario, date_inicio_mes):    
     
     saldo_inicial = 0
+    if request.method == "GET":
+        ingresos_saldo_inicio = Ingresos.objects.filter(
+            usuario = usuario, 
+            date_ingreso__lt = date_inicio_mes    
+            )
+
+        egresos_saldo_inicio = Egresos.objects.filter(
+            usuario = usuario, 
+            date_egreso__lt = date_inicio_mes          
+            )
+
+        ingresos_saldo_monto= 0
+        for i in ingresos_saldo_inicio:                
+            ingresos_saldo_monto += i.monto
+
+        egresos_saldo_monto = 0
+        for e in egresos_saldo_inicio:                
+            egresos_saldo_monto += e.monto
+
+    saldo_inicial = ingresos_saldo_monto + egresos_saldo_monto
+    return saldo_inicial
+
+#=================================================
+def query_mes(request):    
+    
+    usuario = obtener_usuario(request)
+    date_inicio_mes, year, month, month_year = fecha_inicio_mes(request)
+    
+    if not date_inicio_mes:
+    
+        return render(request, "core/movimientos/query_mes.html", {
+            "form": month_year,
+            "usuario": usuario,
+            "saldo_inicial": 0,
+            "movimientos": [],
+            "saldo_final": 0
+        })
+           
+    
+    saldo_inicial = saldo_inicial_query(request, usuario, date_inicio_mes)
+        
     saldo_final = 0
     movimientos = []
     
-    query_mes = QueryMes_Form(request.GET or None)
-    if request.method == "GET":
+    if request.method == "GET":            
+        ingresos_list = Ingresos.objects.filter(
+            usuario = usuario, 
+            date_ingreso__month = month, 
+            date_ingreso__year = year
+            )
         
-        if query_mes.is_valid():  
-            month = query_mes.cleaned_data["month"]
-            year = query_mes.cleaned_data["year"]
-            date_inicio_mes = date(year, month, 1) 
+        egresos_list = Egresos.objects.filter(
+            usuario = usuario, 
+            date_egreso__month = month, 
+            date_egreso__year = year
+            )                   
 
-    # saldo inicial
-            ingresos_saldo_inicio = Ingresos.objects.filter(
-                usuario = usuario, 
-                date_ingreso__lt = date_inicio_mes    
-                )
+        ingresos_mes_monto= 0
+        for i in ingresos_list:                
+            ingresos_mes_monto += i.monto
+            movimientos.append({
+            "tipo": "Ingreso",
+            "monto": i.monto,
+            "descripcion": i.descripcion,
+            "fecha": i.date_ingreso
+            })
+        
+        egresos_mes_monto = 0
+        for e in egresos_list:                
+            egresos_mes_monto += e.monto
+            movimientos.append({
+            "tipo": "Egreso",
+            "monto": e.monto,
+            "descripcion": e.descripcion,
+            "fecha": e.date_egreso
+            })
+        
+        saldo_final = ingresos_mes_monto + egresos_mes_monto + saldo_inicial
+        movimientos.sort(key=lambda x: x["fecha"])
 
-            egresos_saldo_inicio = Egresos.objects.filter(
-                usuario = usuario, 
-                date_egreso__lt = date_inicio_mes          
-                )
-
-            ingresos_saldo_monto= 0
-            for i in ingresos_saldo_inicio:                
-                ingresos_saldo_monto += i.monto
-
-            egresos_saldo_monto = 0
-            for e in egresos_saldo_inicio:                
-                egresos_saldo_monto += e.monto
-
-            saldo_inicial = ingresos_saldo_monto + egresos_saldo_monto
-
-        # movimientos del mes 
-            
-            ingresos_list = Ingresos.objects.filter(
-                usuario = usuario, 
-                date_ingreso__month = month, 
-                date_ingreso__year = year
-                )
-            
-            egresos_list = Egresos.objects.filter(
-                usuario = usuario, 
-                date_egreso__month = month, 
-                date_egreso__year = year
-                )
-                      
-
-            ingresos_mes_monto= 0
-            for i in ingresos_list:                
-                ingresos_mes_monto += i.monto
-                movimientos.append({
-                "tipo": "Ingreso",
-                "monto": i.monto,
-                "descripcion": i.descripcion,
-                "fecha": i.date_ingreso
-                })
-            
-            egresos_mes_monto = 0
-            for e in egresos_list:                
-                egresos_mes_monto += e.monto
-                movimientos.append({
-                "tipo": "Egreso",
-                "monto": e.monto,
-                "descripcion": e.descripcion,
-                "fecha": e.date_egreso
-                })
-            
-            saldo_final = ingresos_mes_monto + egresos_mes_monto + saldo_inicial
-            movimientos.sort(key=lambda x: x["fecha"])
-
-        else:
-            messages.error(request, "No hay movimientos para el período seleccionado")
+    else:
+        messages.error(request, "No hay movimientos para el período seleccionado")
 
     contexto = {
         "usuario": usuario,        
-        "form": query_mes,
+        "form": month_year,
         "saldo_inicial": saldo_inicial,
         "movimientos": movimientos,
         "saldo_final": saldo_final
